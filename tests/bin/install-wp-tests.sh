e2beb37d5936cd2d39048ccefb17f89441f652d6
Vitus Schuhwerk <vitus.schuhwerk@mailbox.org>
2026-03-30 18:04:26 +0200

    fix(scoped-notify): correct conditional checks for database socket and hostname

--- tests/bin/install-wp-tests.sh
+++ tests/bin/install-wp-tests.sh
@@ -168,12 +168,12 @@ install_db() {
 	local DB_SOCK_OR_PORT=${PARTS[1]};
 	local EXTRA=""
 
-	if [echo "$DB_SOCK_OR_PORT" | grep -q '^[0-9]\{1,\}$'
-		if [[ $(echo $DB_SOCK_OR_PORT | grep -e '^[0-9]\{1,\}$') ]]; then
+	if [[ -n "$DB_HOSTNAME" ]]; then
+		if echo "$DB_SOCK_OR_PORT" | grep -q '^[0-9]\{1,\}$'; then
 			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-		elif [[ ! -z $DB_SOCK_OR_PORT ]] ; then
+		elif [[ -n "$DB_SOCK_OR_PORT" ]]; then
 			EXTRA=" --socket=$DB_SOCK_OR_PORT"
-		elif [[ ! -z $DB_HOSTNAME ]] ; then
+		elif [[ -n "$DB_HOSTNAME" ]]; then
 			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
 		fi
 	fi
